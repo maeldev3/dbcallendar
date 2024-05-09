dbf86a31784a617e8e6838d03d0e02b41ceb9b69
maelDev3 <maeldev3@gmail.com>
2024-05-09 18:35:32 +0200

    modifi server

--- api/server.js
+++ api/server.js
@@ -6,15 +6,26 @@ const server = jsonServer.create()
 
 const router = jsonServer.router('db.json')
 
+const missionsData = JSON.parse(fs.readFileSync(join('/tmp', 'db.json'), 'utf8')); 
+
+
+
+
+
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
 }))
 server.use(router)
+
+
+server.get('/missions', (req, res) => {
+   res.json(missionsData);
+});
+
 server.listen(3000, () => {
     console.log('JSON Server is running')
 })
