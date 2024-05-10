2d7c1da13ae5368a942ea501d34b6de4bc18ac5f
maelDev3 <maeldev3@gmail.com>
2024-05-10 06:46:41 +0200

    config server

--- api/server.js
+++ api/server.js
@@ -12,18 +12,6 @@ const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 // Add this before server.use(router)
-// Add a check to ensure 'db.json' file exists and is writable
-// try {
-//     // Check if the file exists
-//     fs.accessSync('db.json', fs.constants.R_OK | fs.constants.W_OK);
-// } catch (err) {
-//     // If the file does not exist or is not writable, handle the error
-//     console.error('Error accessing or writing to db.json file:', err);
-//     process.exit(1); // Exit the process with an error code
-// }
-
-
-
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
 }))
