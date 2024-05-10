46bc8723d0009636c8be91af236ad7db5c4a15b5
maelDev3 <maeldev3@gmail.com>
2024-05-10 07:30:04 +0200

    server

--- api/server.js
+++ api/server.js
@@ -1,23 +1,9 @@
 
 const jsonServer = require('json-server')
-var fs = require("fs");
+
 const server = jsonServer.create()
-// const router = jsonServer.router('db.json')
+const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
-
-
-
-// Replace with the URL of your separate server hosting db.json
-const dbUrl = 'https://dbcallendar.vercel.app/';
-
-async function getDbData() {
-  const response = await fetch(dbUrl);
-  const data = await response.json();
-  return data;
-}
-
-const router = jsonServer.router(getDbData); 
-
 
 
 server.use(middlewares)
@@ -26,9 +12,6 @@ server.use(jsonServer.rewriter({
     '/api/*': '/$1',
 }))
 server.use(router)
-
-
-
 
 
 server.listen(3000, () => {
