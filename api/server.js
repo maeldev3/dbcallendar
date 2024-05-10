4d965cf4a6386195ef721b0070e639cbec106287
maelDev3 <maeldev3@gmail.com>
2024-05-10 08:29:11 +0200

    d

--- api/server.js
+++ api/server.js
@@ -1,18 +1,9 @@
 
 const jsonServer = require('json-server')
-const fs = require("fs")
-const os = require("os")
 
 const server = jsonServer.create()
-const router = jsonServer.router(path.resolve(os.tmpdir() + "/db.json"));
-
-// const router = jsonServer.router('db.json')
+const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
-
-// Copy the db.json file to the temporary directory (optional)
-fs.copyFile('db.json', os.tmpdir() + '/db.json')
-  .then(() => console.log('Copy file succeeded to', os.tmpdir()))
-  .catch(err => console.error('Error copying file:', err));
 
 
 server.use(middlewares)
